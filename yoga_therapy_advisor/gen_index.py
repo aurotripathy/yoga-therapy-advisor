864537b0ce6e33de0c0c9f113d1efb635f68aad7
Auro Tripathy <ipserv@yahoo.com>
2023-02-03 15:18:51 -0800

    initial

---
+++ yoga_therapy_advisor/gen_index.py
@@ -0,0 +1,43 @@
+from utils import (
+    parse_docx,
+    parse_pdf,
+    parse_txt,
+    search_docs,
+    embed_docs,
+    text_to_docs,
+    get_answer,
+    get_sources,
+    wrap_text_in_html,
+)
+from langchain.vectorstores import FAISS, VectorStore
+from langchain.docstore.document import Document
+from typing import List, Dict, Any
+from langchain.embeddings import OpenAIEmbeddings
+import os
+# from pudb import set_trace
+
+openai_api_key = os.environ["OPENAI_API_KEY"]
+
+def embed_docs(docs: List[Document]) -> VectorStore:
+    """Embeds a list of Documents and returns a FAISS index
+       Deals with the OpenAI API key externally (ebv var)
+    """
+
+    # Embed the chunks
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)  # type: ignore
+    index = FAISS.from_documents(docs, embeddings)
+
+    return index
+
+
+file_to_be_indexed = '/media/auro/Dropbox/My Ideas/yoga advisor/OCRed/Chapters/Chapter 1 - Therapeutic Yoga and Its Essentials.docx'
+
+text = text_to_docs(file_to_be_indexed)
+index = embed_docs(text)
+print(f'saving...')
+index.save_local('./saved_index')
+print(f'loading...')
+index.save_local('./saved_index')
+
+
+
